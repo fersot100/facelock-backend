1db30053b506dbdf5b422b1b38a680ccb2d1d48f
Juan Erazo <fersot100@yahoo.com>
2017-04-02 09:52:45 -0700

    Does this work?

--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ module.exports = function (sequelize, DataTypes) {
 		password: {
 			type: DataTypes.STRING,
 			allowNull: false,
-			unique: false
+			unique: false						
 		},
 		pi: {
 			type: DataTypes.STRING
--- server.js
+++ server.js
@@ -11,10 +11,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Access-Control-Allow-Origin, Content-Type,");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
- });
+});
 
 app.get('/', function(req, res, next) {
   // Handle the get for this route
