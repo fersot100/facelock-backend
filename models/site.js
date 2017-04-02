9624c34ebc52ffafc8b278dcb1e259bd49e9cc82
Juan Erazo <fersot100@yahoo.com>
2017-04-02 08:36:08 -0700

    Completely destroyed all validation this is broken

--- models/site.js
+++ models/site.js
@@ -2,20 +2,15 @@ module.exports = function(sequelize, DataTypes) {
 	var site = sequelize.define('site', {
 		url: {
 			type: DataTypes.STRING,
-			isNull: false,
-			validate: {
-				isURL: true
-			}
+
 		},
 		username: {
 			type: DataTypes.STRING,
-			allowNull: false,
-			len: [2, 254]
 
 		},
 		password: {
 			type: DataTypes.STRING,
-			allowNull: false
+
 		}
 	}, {
 		classMethods: {
