da3a48c42165d5d86640e8c353c2f9e49a42328a
Juan Erazo <fersot100@yahoo.com>
2017-04-02 08:12:41 -0700

    Changed CORS - JP

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.all('/', function(req, res, next) {
+app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
