9db1b6a68712799aa1789256a8467f09dea08614
Juan Erazo <fersot100@yahoo.com>
2017-04-02 08:24:30 -0700

    geh

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Access-Control-Allow-Origin, Content-Type,");
   next();
  });
 
